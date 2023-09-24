b4dbbcdfcc63d2eb8dd7e44ca77225853c5ba5a3
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2023-09-24 15:08:41 +0500

    update 2

--- index.js
+++ index.js
@@ -26,17 +26,12 @@ app.use(bodyParser.json());
 
 
 app.post("/submit-welcome-note", (req, res) => {
-  // Extract the form data from the request body
-  const {  userName , userEmail} = req.body;
+  
+  const {  userName , userEmail , address , phone , date , time , itemName , itemQuantity} = req.body;
   console.log(userEmail);
-  console.log(userName);
-  console.log("test");
-
-  // Send an email with the form details using Nodemailer or your preferred email library
-  // Here's an example using Nodemailer
+  
   const nodemailer = require("nodemailer");
 
-// Create a transporter object for sending the email
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
@@ -51,28 +46,37 @@ const transporter = nodemailer.createTransport({
 const storeMailOptions = {
   from: userEmail,
   to: "vascularbundle43@gmail.com",
-  subject: "Fantasy MMAdness",
+  subject: "New Cafe Order",
   html: `
-    <h2 style="color: #ff523b;">Fantasy MMAdness</h2>
+    <h2 style="color: #ff523b;">SweetSpot Desserts</h2>
     <hr style="border: 1px solid #ccc;">
-    <p>Another user added</p>
+    <h3>Order Details:</h3>
+    <p><strong>Name:</strong> ${userName}</p>
+    <p><strong>Email:</strong> ${userEmail}</p>
+    <p><strong>Address:</strong> ${address}</p>
+    <p><strong>Phone:</strong> ${phone}</p>
+    <p><strong>Date:</strong> ${date}</p>
+    <p><strong>Time:</strong> ${time}</p>
+    <p><strong>Item Name:</strong> ${itemName}</p>
+    <p><strong>Item Quantity:</strong> ${itemQuantity}</p>
   `,
 };
-
 const userMailOptions = {
   from: "wajih786hassan@gmail.com",
   to: userEmail,
-  subject: "Fantasy MMAdness",
+  subject: "SweetSpot Desserts",
   html: `
-  <h2 style="color: red;">Welcome to Fantasy MMAdness</h2>
-  <p>Hello ${userName},</p>
-  <p>Thank you for signing up for Fantasy MMAdness! We're excited to have you on board and join our community of MMA enthusiasts.</p>
-  <p>With Fantasy MMAdness, you can create and manage your own fantasy MMA leagues, make predictions, and compete with others to prove your MMA knowledge.</p>
-  <p>Your journey into the world of MMA fantasy gaming begins now. Get ready for some adrenaline-pumping action and fierce competition!</p>
-  <p>If you have any questions or need assistance, don't hesitate to reach out to our support team at support@fantasymmadness.com.</p>
-  <p>Once again, welcome to Fantasy MMAdness, ${userName}! Let the battles begin!</p>
-  <p>Best regards,</p>
-  <p>The Fantasy MMAdness Team</p> 
+    <h2>Welcome to SweetSpot Desserts, ${userName}!</h2>
+    <p>Thank you for placing your order with us. Here are the details of your order:</p>
+    <h3>Order Details:</h3>
+    <p><strong>Address:</strong> ${address}</p>
+    <p><strong>Phone:</strong> ${phone}</p>
+    <p><strong>Date:</strong> ${date}</p>
+    <p><strong>Time:</strong> ${time}</p>
+    <p><strong>Item Name:</strong> ${itemName}</p>
+    <p><strong>Item Quantity:</strong> ${itemQuantity}</p>
+    
+    <p>Your order has been received and will be processed shortly. Thank you for choosing SweetSpot Desserts!</p>
   `,
 };
 
